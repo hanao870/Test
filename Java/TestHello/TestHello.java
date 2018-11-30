7b7671d7a8850e3c8671338c748223dc1f524a43
hanao870 <hanackholehole@gmail.com>
2018-11-30 10:52:04 +0900

    branch commit
    test for branch development

--- Java/TestHello/TestHello.java
+++ Java/TestHello/TestHello.java
@@ -2,5 +2,12 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("aiueo");
         System.out.println("あいうえお");
+
+        HelloStr("World!");
+    }
+
+    private static void HelloStr(String w)
+    {
+        System.out.println("Hello " + w);
     }
 }
