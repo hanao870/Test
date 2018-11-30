06d6d5e33f22b98cd248150c0bbd7803ea0be233
hanao870 <hanackholehole@gmail.com>
2018-11-30 11:27:05 +0900

    Add devel-2nd
    Test for loop

--- Java/TestHello/TestHello.java
+++ Java/TestHello/TestHello.java
@@ -2,5 +2,13 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("aiueo");
         System.out.println("あいうえお");
+
+        HelloForLoop();
+    }
+
+    private static void HelloForLoop()
+    {
+        for(int i = 0; i < 10; i++)
+            System.out.println(i + " : Hello");
     }
 }
